11d7d1fb49412ac4bfcfae3c27a1b0cb2768a065
Alex <adam3065nd123@gmail.com>
2023-03-31 16:33:26 +0800

    changes

--- 7_read_csv.py
+++ 7_read_csv.py
@@ -6,11 +6,18 @@ import pandas as pd
 
 # 1. Input
 raw_data = pd.read_csv("Menu.csv")
-print(raw_data)
-print(raw_data.info())
+print(raw_data)                 #print table
+print(raw_data.info())          #print table info
 
 # 2. Process
-total = raw_data["Price"].sum()
+total = raw_data["Price"].sum() #Calculate column sum
+
+raw_data["Price"].var()                       #Calculate column Var
+raw_data["Price"].std()                       #Calculate column std
+raw_data["Price"].sort_values()               #Sort column by values
+raw_data.sort_values("Price")                 #Sort column by values: show all columns
+raw_data.sort_values("Price",ascending=False) #Sort column by values: descending order
+
 
 # 3. Output
 print(f"Total: {total}")
